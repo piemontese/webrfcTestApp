14c983ae89e9432a6a15e4fa328077279db38f6b
= <pie.piemontese@gmail.com>
2018-02-06 23:19:20 +0100

    Continue ata table detail

--- src/app/components/commons/data-table-detail/data-table-detail.component.ts
+++ src/app/components/commons/data-table-detail/data-table-detail.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { DataTableComponent } from '../../data-table/data-table.component';
+
+import { DialogService } from '../../../services/dialog.service';
+
+interface Button {
+  caption: string;
+  color: string;
+  close: boolean;
+}
+
 @Component({
   selector: 'app-data-table-detail',
   templateUrl: './data-table-detail.component.html',
   styleUrls: ['./data-table-detail.component.css']
 })
-export class DataTableDetailComponent implements OnInit {
-	@Input() title = "Title";
+export class DataTableDetailComponent extends DataTableComponent implements OnInit {
+  @Input() title = 'Title';
+  public dialogButtons: Button[];
+  public response: String = '';
 
-  constructor() { }
+  constructor( public dialogService: DialogService ) {
+     super( dialogService );
+  }
 
-  ngOnInit() {
+  onClick( response: string ) {
+    this.response = response;
+  }
+
+ ngOnInit() {
   }
 
 }
